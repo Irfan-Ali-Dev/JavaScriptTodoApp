b109f72ef11a6173e8d462f31d3b70a06f8d1aab
Irfan-Ali-Dev <yousafbuilders.it@gmail.com>
2025-11-09 10:26:44 +0500

    updated

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 console.log("Hello JavaScript Todo App");
+
+const input = document.querySelector("#input");
+const ol = document.querySelector("#ol");
+
+const inputTodo = [];
+
+function renderTodo() {   
+    ol.innerHTML = "";
+    for (let i = 0; i < inputTodo.length; i++) {
+        ol.innerHTML += `
+        <li>
+            ${inputTodo[i]} 
+            <button onclick="editTodo(${i})">Edit</button>
+            <button onclick="deleteTodo(${i})">Delete</button>
+        </li>`;
+    }
+}
+function addTodo() {
+    inputTodo.push(input.value);
+
+    renderTodo();
+    input.value = "";    
+}
+function editTodo(index) {
+    console.log("Todo Task Edited", index);
+    const updatedVal = prompt("Enter Updated Value Please", inputTodo[index]);
+    inputTodo[index] = updatedVal
+    
+    renderTodo();
+}
+function deleteTodo(index) {
+    console.log("Todo Task Deleted");
+    inputTodo.splice(index, 1);
+
+    renderTodo();
+}
